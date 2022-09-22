cc4da5b55e18b1546d8c0ad549c317f5506f7b48
pab-h <dev.pab.2020@gmail.com>
2022-09-22 07:31:40 -0300

    update particle system

--- src/ParticleSystem.py
+++ src/ParticleSystem.py
@@ -1,2 +1,19 @@
+from src.Point import MassPoint
+from src.Point import Point
+
 class ParticleSystem:
-    pass
+    def __init__(self, particles: list[MassPoint] = []) -> None:
+        self.__particles = particles
+        self.__massCenter = self.calcMassCenter()
+
+    def addParticles(self, particles: list[MassPoint]) -> None:
+        self.__particles += particles
+        self.__massCenter = self.calcMassCenter()
+
+    def calcMassCenter(self) -> Point:
+        return Point(10, 10, 10)
+
+    @property
+    def massCenter(self) -> Point:
+        return self.__massCenter
+
