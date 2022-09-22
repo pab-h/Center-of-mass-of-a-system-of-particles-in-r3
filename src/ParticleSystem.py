852b866fcf3835e6589de3afb22a3332b51d4457
pab-h <dev.pab.2020@gmail.com>
2022-09-22 10:32:27 -0300

    calc mass center and update system

--- src/ParticleSystem.py
+++ src/ParticleSystem.py
@@ -1,17 +1,43 @@
+from functools import reduce
 from src.Point import MassPoint
 from src.Point import Point
 
 class ParticleSystem:
     def __init__(self, particles: list[MassPoint] = []) -> None:
         self.__particles = particles
+        self.__massCenter: Point = Point(0, 0, 0)
+        self.__systemMass: float = 0
+        self.__update()
+
+    def __update(self) -> None:
+        self.__systemMass = self.calcSystemMass()
         self.__massCenter = self.calcMassCenter()
 
     def addParticles(self, particles: list[MassPoint]) -> None:
         self.__particles.extend(particles)
-        self.__massCenter = self.calcMassCenter()
+        self.__update()
 
     def calcMassCenter(self) -> Point:
-        return Point(10, 10, 10)
+        def reduceX(accumulated: float, massPoint: MassPoint) -> float:
+            return accumulated + massPoint.mass * massPoint.x
+
+        def reduceY(accumulated: float, massPoint: MassPoint) -> float:
+            return accumulated + massPoint.mass * massPoint.y
+
+        def reduceZ(accumulated: float, massPoint: MassPoint) -> float:
+            return accumulated + massPoint.mass * massPoint.z
+
+        return Point(
+            x = reduce(reduceX, self.__particles, 0) / self.__systemMass,
+            y = reduce(reduceY, self.__particles, 0) / self.__systemMass,
+            z = reduce(reduceZ, self.__particles, 0) / self.__systemMass
+        )
+
+    def calcSystemMass(self) -> float:
+        def reduceMassParticles(accumulated: float, current: MassPoint) -> float:
+            return accumulated + current.mass
+
+        return reduce(reduceMassParticles, self.__particles, 0)
 
     @property
     def massCenter(self) -> Point:
